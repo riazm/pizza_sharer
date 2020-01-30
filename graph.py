37d9296c231160129f0c66277ebd7ad12b43ebc5
Riaz Moola <riaz.moola@fluidic.com>
2020-01-30 16:42:44 +0000

    works for toy example but edgar weirdo and oddboi brek it

--- graph.py
+++ graph.py
@@ -18,7 +18,9 @@ rachel = PizzaWanter("rachel", ["courgette"])
 monkey = PizzaWanter("monkey", ["funghi"])
 tort = PizzaWanter("tort", ["onion", "cabbage"])
 chicken = PizzaWanter("chicken", ["cabbage"])
-
+edgar = PizzaWanter("edgar", ["mozarella"])
+weirdo = PizzaWanter("weirdo", ["pine apple"])
+oddboi = PizzaWanter("oddBoi", ["mozarella", "funghi", "courgette", "onion", "cabbage"])
 
 def createGraph(pizzaWanters):
     pizza_graph = {}
@@ -29,27 +31,24 @@ def createGraph(pizzaWanters):
                 for wanted_pizza in wanter.wanted_pizzas:
                     if wanted_pizza in sharer.wanted_pizzas:
                         pizza_graph[wanter.name].append(sharer.name)
-                        # print(wanter.name, "wants", wanted_pizza)
-                        # print(sharer.name, "willing to share", sharer.wanted_pizzas)
     return(pizza_graph)
 
 
 def bruteForce(want_graph):
-    pair_graph = {}
+    matching_edges = {}
     taken = []
     for wanter in want_graph:
         if wanter not in taken:
-            want_graph[wanter] = set(want_graph[wanter]) - set(taken)
             for sharer in want_graph[wanter]:
-                pair_graph[wanter] = sharer
-                taken.append(sharer)
-                taken.append(wanter)
+                if sharer not in taken:
+                    matching_edges[wanter] = sharer
+                    taken.append(sharer)
+                    taken.append(wanter)
                 break
-    return pair_graph
+    return matching_edges
 
 
 def getPath(want_graph, path):
-    print(want_graph)
     wanter = path[-1]
     for sharer in want_graph[wanter]:
         if sharer not in path:
@@ -59,18 +58,50 @@ def getPath(want_graph, path):
     return path
 
 
-def augmentingPath(want_graph, initial_flow):
+def augmentingPath(want_graph, initial_match):
     for wanter in want_graph:
-        if wanter not in initial_flow.keys() \
-           and wanter not in initial_flow.values():
-            return getPath(want_graph, [wanter])
+        if wanter not in initial_match.keys() \
+           and wanter not in initial_match.values():
+            path = getPath(want_graph, [wanter])
+            if len(path) > 1:
+                return path
+    return None
 
+
+def subtractAugmentingPath(want_graph, initial_match, augmenting_path):
+    augmented_match = {}
+    for share_pair in zip(augmenting_path, augmenting_path[1:]):
+        wanter = share_pair[0]
+        sharer = share_pair[1]
+        if wanter in initial_match.keys():
+            if initial_match[wanter] == sharer:
+                print("Error, wanter - sharer path already exists", wanter, sharer)
+        elif wanter in initial_match.values():
+            for node in want_graph[wanter]:
+                connection = initial_match.get(node)
+                if connection == wanter:
+                    initial_match.pop(node)
+        else:
+            augmented_match[wanter] = sharer
+
+    initial_match.update(augmented_match)
+    return initial_match
 
 graph = createGraph([riaz, scott, joulia, artemis,
-                     rachel, monkey, tort, chicken])
-print(graph)
-pair_flow = bruteForce(graph)
-print(pair_flow)
-augmenting_path = augmentingPath(graph, pair_flow)
-print(augmenting_path)
+                     rachel, monkey, tort, chicken,
+                     edgar, weirdo, oddboi])
+print("graph", graph)
+initial_matching_edges = bruteForce(graph)
+print("initial matching", initial_matching_edges)
+
+while True:
+    augmenting_path = augmentingPath(graph, initial_matching_edges)
+    print("augmenting path", augmenting_path)
+    if augmenting_path == None:
+        break
+    else: 
+        matching_edges = subtractAugmentingPath(graph, initial_matching_edges, augmenting_path)
+
+
+print("subtracted_path", matching_edges)
 
